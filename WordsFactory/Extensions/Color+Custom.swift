5ad119fbad23dba63015bc180a01a30c3212dab7
kperegudova <kperegudova@kode-t.ru>
2021-11-05 15:05:01 +0700

    onboarding

--- WordsFactory/Extensions/Color+Custom.swift
+++ WordsFactory/Extensions/Color+Custom.swift
@@ -6,3 +6,9 @@
 //
 
 import SwiftUI
+
+extension Color {
+  static let accent = Color(UIColor(red: 0.892, green: 0.338, blue: 0.163, alpha: 1))
+  static let inc = Color(UIColor(red: 0.233, green: 0.226, blue: 0.212, alpha: 1))
+  static let incGray = Color(UIColor(red: 0.471, green: 0.456, blue: 0.427, alpha: 1))
+}
---
+++ WordsFactory/Sign UP/SignUpView.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpView.swift
+//  WordsFactory
+//
+//  Created by Кристина Перегудова on 05.11.2021.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
